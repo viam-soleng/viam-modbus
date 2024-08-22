2b72c422b810ec0b635c6cf75d4e9d161c084f30
Alan Davidson <alan@viam.com>
2024-08-22 16:39:37 -0400

    make the main function ModularMain() (#11)
    This is really just a way to show off how to use `module.ModularMain()` to avoid copying boilerplate to every module we write.
    
    The idea behind the function is that you give it the name of your logger and as many (API, model) pairs as you want, and it stands up the rest of the module for you.
    
    There is no direct benefit to updating all the old modules, but I do think this will make writing new modules faster and easier. (If people copy and paste the old modules as the start of the new ones, maybe they should be updated after all.)
    
    This module still compiles and starts up, though I haven't tested it further than that.
    
    
    * update rdk to 0.37.0
    
    * go mod tidy
    
    * use ModularMain

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"context"
-
 	"go.viam.com/rdk/components/board"
 	"go.viam.com/rdk/components/sensor"
-	"go.viam.com/rdk/logging"
 	"go.viam.com/rdk/module"
-	"go.viam.com/utils"
+	"go.viam.com/rdk/resource"
 
 	modbus_board "viam-modbus/modbus_board"
 	modbus_sensor "viam-modbus/modbus_sensor"
@@ -15,31 +12,8 @@ import (
 )
 
 func main() {
-	utils.ContextualMain(mainWithArgs, module.NewLoggerFromArgs(module_utils.LoggerName))
-}
-
-func mainWithArgs(ctx context.Context, args []string, logger logging.Logger) (err error) {
-	custom_module, err := module.NewModuleFromArgs(ctx, logger)
-	if err != nil {
-		return err
-	}
-
-	err = custom_module.AddModelFromRegistry(ctx, sensor.API, modbus_sensor.Model)
-	if err != nil {
-		return err
-	}
-
-	err = custom_module.AddModelFromRegistry(ctx, board.API, modbus_board.Model)
-	if err != nil {
-		return err
-	}
-
-	err = custom_module.Start(ctx)
-	defer custom_module.Close(ctx)
-	if err != nil {
-		return err
-	}
-
-	<-ctx.Done()
-	return nil
+	module.ModularMain(module_utils.LoggerName,
+	                   resource.APIModel{sensor.API, modbus_sensor.Model},
+	                   resource.APIModel{board.API, modbus_board.Model},
+	                   )
 }
