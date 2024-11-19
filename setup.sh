26b835026eefc905b019be477985c4ca10eef321
Pete Garafano <pete@viam.com>
2024-11-19 11:46:57 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# List of required packages
-declare -a extra_packages=(
-    "build-essential"
-)
-
-# Check for each package and add missing ones to the list
-for package in "${extra_packages[@]}"; do
-    if ! dpkg -l | grep -q "^ii\s*$package\s"; then
-        printf "Package %s is missing\n" "$package"
-        return 1
-    fi
-done
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    # List of required packages
+    declare -a extra_packages=(
+        "build-essential"
+    )
+    # Linux: Use dpkg to check for packages
+    for package in "${extra_packages[@]}"; do
+        if ! dpkg -l | grep -q "^ii\s*$package\s"; then
+            printf "Package %s is missing on Linux\n" "$package"
+            exit 1
+        fi
+    done
+fi
