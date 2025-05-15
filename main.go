0ad1aa00f2f9e7fa2ca530fb59445b7e39d7bc8c
John Walicki <johnwalicki@gmail.com>
2025-05-15 14:16:06 -0400

    Remove non-standard viam-utils dependency
    Signed-off-by: John Walicki <johnwalicki@gmail.com>

--- main.go
+++ main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	moduleutils "github.com/thegreatco/viamutils/module"
 	"go.viam.com/rdk/components/board"
 	"go.viam.com/rdk/components/sensor"
 	"go.viam.com/rdk/module"
-	viamutils "go.viam.com/utils"
+	"go.viam.com/rdk/resource"
+	//viamutils "go.viam.com/utils"
 
-	modbus_board "viam-modbus/modbus_board"
+	modbus_board  "viam-modbus/modbus_board"
 	modbus_sensor "viam-modbus/modbus_sensor"
-	"viam-modbus/utils"
+	//module_utils  "viam-modbus/utils"
 )
 
 func main() {
-	moduleutils.AddModularResource(sensor.API, modbus_sensor.Model)
-	moduleutils.AddModularResource(board.API, modbus_board.Model)
-	viamutils.ContextualMain(moduleutils.RunModule, module.NewLoggerFromArgs(utils.LoggerName))
+	// ModularMain can take multiple APIModel arguments, if your module implements multiple models.
+	module.ModularMain( 
+		resource.APIModel{board.API,  modbus_board.Model},
+		resource.APIModel{sensor.API, modbus_sensor.Model},
+	)
 }
