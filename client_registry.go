3d22808c99ba8fce727e881f8830abf915d90398
felixreichenbach <felix.reichenbach@viam.com>
2025-07-17 16:56:23 +0200

    Update client_registry.go

--- client_registry.go
+++ client_registry.go
@@ -1,76 +1,45 @@
 package viammodbus
 
 import (
-	"context"
 	"fmt"
 	"sync"
-
-	"go.viam.com/rdk/logging"
 )
 
-var GlobalClientRegistry *clientRegistry = &clientRegistry{clients: map[string]Client{}}
-
-type Client interface {
-	Start() error
-	Close(ctx context.Context) error
-}
+var GlobalClientRegistry *clientRegistry = &clientRegistry{clients: map[string]*modbusClient{}}
 
 type clientRegistry struct {
-	clients map[string]Client
+	clients map[string]*modbusClient
 	mu      sync.Mutex
 }
 
-func (cr *clientRegistry) Add(name string, c Client) error {
+func (cr *clientRegistry) Add(name string, client *modbusClient) error {
 	cr.mu.Lock()
 	defer cr.mu.Unlock()
 
-	_, got := cr.clients[name]
+	client, got := cr.clients[name]
 	if got {
-		return fmt.Errorf("cannot add duplicate readers with name [%s]", name)
+		//TODO: Make sure the mobus client is closed before replacing it
 	}
-
-	cr.clients[name] = c
+	cr.clients[name] = client
 	return nil
 }
 
 func (cr *clientRegistry) Remove(name string) {
 	cr.mu.Lock()
 	defer cr.mu.Unlock()
-
+	if _, got := cr.clients[name]; got {
+		// TODO: Close the client if it is not already closed
+	}
 	delete(cr.clients, name)
 }
 
-func (cr *clientRegistry) GetOrCreate(src string, logger logging.Logger) (Client, error) {
+func (cr *clientRegistry) Get(name string) (*modbusClient, error) {
 	cr.mu.Lock()
 	defer cr.mu.Unlock()
 
-	c, got := cr.clients[src]
+	c, got := cr.clients[name]
 	if !got {
-		if src == "" {
-			return nil, fmt.Errorf("tried to create client with blank name")
-		}
-		c = CreateClient(src, logger)
-		cr.clients[src] = c
-		err := c.Start()
-		if err != nil {
-			return nil, err
-		}
+		return nil, fmt.Errorf("no client with name [%s] found", name)
 	}
 	return c, nil
-}
-
-// TODO: Implement a default client that does nothing, so we can always return a client.
-type defaultClient struct{}
-
-func (d *defaultClient) Start() error {
-	return nil
-}
-
-func (d *defaultClient) Close(ctx context.Context) error {
-	return nil
-}
-
-func CreateClient(src string, logger logging.Logger) Client {
-	// VIAMBOAT: https://github.com/viam-labs/viamboat/blob/83aed8a52948aa6f41e0977cbc9f07921a6a801b/reader.go#L269
-	return &defaultClient{}
 }
